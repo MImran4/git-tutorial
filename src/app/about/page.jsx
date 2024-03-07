69864a584f5c09587fc379abf582caf7ae08decf
imran.iinv <muhammad-imran@iinv.tech>
2024-03-07 16:07:24 +0500

    msg

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 
 const About = () => {
   const val = useSelector((state) => state.counter.value);
-  return <div>hello</div>;
+  return <div>hello=</div>;
 };
 
 export default About;
